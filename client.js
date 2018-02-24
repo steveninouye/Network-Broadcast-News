47fb73a7087fab3c5d6ffb83999e3edbc209dfd7
Steven Inouye <steveninouye@msn.com>
2018-02-24 13:50:13 -1000

    finished username, admin broadcast, not allowing user to log in with "admin"

--- client.js
+++ client.js
@@ -1,6 +1,20 @@
 const net = require('net');
+process.stdin.setEncoding('utf8');
 
 const server = net.createConnection(6969, '127.0.0.1', socket => {
-  server.write(server.address().address.toString());
-  server.write(server.address().port.toString());
+
+  server.Address = server.address().address;
+  server.Port = server.address().port;
+  console.log(server.Port);
+
+  server.on('data', data => {
+    console.log(data.toString());
+  })
+
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if(chunk !== null) {
+      server.write(chunk.toString());
+    }
+  })
 });
--- server.js
+++ server.js
@@ -1,16 +1,74 @@
 const net = require('net');
 
+let chatGroup = [];
+
+process.stdin.setEncoding('utf8');
+
+
 var server = net.createServer(socket => {
-  socket.write('You are connected');
+  socket.Address = socket.address().address;
+  socket.Port = socket.address().port;
+  chatGroup.push(socket);
+  socket.write(`You are connected on address: ${socket.Address} port: ${socket.Port}\r\nSet Your Username: `);
+
+
   socket.on('data', data => {
-    console.log(data.toString());
+    let message = false;
+    //stake out any new lines from data
+    data = data.toString().replace(/\n/g,"");  
+
+    if (!socket.name) {
+      let userExists = false;
+      //check if there are any users with that name already
+      chatGroup.forEach((e,i) => {        
+        if (e.name === data) {
+          socket.write(`"${data}" is already being used by another user`)
+          userExists = true;
+        } else if (data.toLowerCase().indexOf('admin') !== -1){
+          socket.write(`Not able to use username with "admin"`);
+          userExists = true;
+        }
+      });
+      if(!userExists) {
+        socket.name = data;
+        message = `****${socket.name} has signed on****`
+      }      
+    } else {
+      message = `${socket.name} : ${data}`;
+    }  
+    if (message){console.log(message);
+    chatGroup.forEach((e,i) => {
+      if(chatGroup[i] !== socket) {
+        e.write(message);
+      }
+    });}
   });
-  socket.on('end', () => {
-    socket.write('BAI');
-  });
+
   socket.on('error', err => {
-    console.log('Disconnected');
+    console.log(`Server is currently down`);
   });
+
+  socket.on('end', () => {
+    socket.write('bye');
+    chatGroup.forEach(e=> {
+      e.write(`${socket.name} disconnected`);
+    });
+    let indexOfSocket = chatGroup.indexOf(socket)
+      if(indexOfSocket !== -1) {
+        chatGroup.splice(indexOfSocket, 1);
+        console.log(chatGroup.length);
+      }
+  });
+
+
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
+    if(chunk !== null) {
+      chatGroup.forEach(e => {
+        e.write(`[ADMIN] : ${chunk}`);
+      })
+    }
+  })
 });
 
 server.listen(6969, '127.0.0.1', () => {
