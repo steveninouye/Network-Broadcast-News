8bc0e13add2f053f12c87f79792a8c0e401d12ad
Steven Inouye <steveninouye@msn.com>
2018-02-24 14:53:29 -1000

    finished kicking user with \kick

--- client.js
+++ client.js
@@ -2,19 +2,18 @@ const net = require('net');
 process.stdin.setEncoding('utf8');
 
 const server = net.createConnection(6969, '127.0.0.1', socket => {
-
   server.Address = server.address().address;
   server.Port = server.address().port;
   console.log(server.Port);
 
   server.on('data', data => {
     console.log(data.toString());
-  })
+  });
 
   process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
-    if(chunk !== null) {
+    if (chunk !== null) {
       server.write(chunk.toString());
     }
-  })
+  });
 });
--- server.js
+++ server.js
@@ -4,71 +4,91 @@ let chatGroup = [];
 
 process.stdin.setEncoding('utf8');
 
-
 var server = net.createServer(socket => {
   socket.Address = socket.address().address;
   socket.Port = socket.address().port;
   chatGroup.push(socket);
-  socket.write(`You are connected on address: ${socket.Address} port: ${socket.Port}\r\nSet Your Username: `);
-
+  socket.write(
+    `You are connected on address: ${socket.Address} port: ${
+      socket.Port
+    }\r\nSet Your Username: `
+  );
 
   socket.on('data', data => {
     let message = false;
     //stake out any new lines from data
-    data = data.toString().replace(/\n/g,"");  
+    data = data.toString().replace(/\n/g, '');
 
     if (!socket.name) {
       let userExists = false;
       //check if there are any users with that name already
-      chatGroup.forEach((e,i) => {        
+      chatGroup.forEach((e, i) => {
         if (e.name === data) {
-          socket.write(`"${data}" is already being used by another user`)
+          socket.write(`"${data}" is already being used by another user`);
           userExists = true;
-        } else if (data.toLowerCase().indexOf('admin') !== -1){
+        } else if (data.toLowerCase().indexOf('admin') !== -1) {
           socket.write(`Not able to use username with "admin"`);
           userExists = true;
         }
       });
-      if(!userExists) {
+      if (!userExists) {
         socket.name = data;
-        message = `****${socket.name} has signed on****`
-      }      
+        message = `****${socket.name} has signed on****`;
+      }
     } else {
       message = `${socket.name} : ${data}`;
-    }  
-    if (message){console.log(message);
-    chatGroup.forEach((e,i) => {
-      if(chatGroup[i] !== socket) {
-        e.write(message);
-      }
-    });}
+    }
+    if (message) {
+      console.log(message);
+      chatGroup.forEach((e, i) => {
+        if (chatGroup[i] !== socket) {
+          e.write(message);
+        }
+      });
+    }
   });
 
   socket.on('error', err => {
-    console.log(`Server is currently down`);
+    console.log(`Error Occured`);
   });
 
   socket.on('end', () => {
     socket.write('bye');
-    chatGroup.forEach(e=> {
+    chatGroup.forEach(e => {
       e.write(`${socket.name} disconnected`);
     });
-    let indexOfSocket = chatGroup.indexOf(socket)
-      if(indexOfSocket !== -1) {
-        chatGroup.splice(indexOfSocket, 1);
-        console.log(chatGroup.length);
-      }
+    let indexOfSocket = chatGroup.indexOf(socket);
+    if (indexOfSocket !== -1) {
+      chatGroup.splice(indexOfSocket, 1);
+    }
   });
 
-
   process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if(chunk !== null) {
-      chatGroup.forEach(e => {
-        e.write(`[ADMIN] : ${chunk}`);
-      })
+    let chunk = process.stdin.read();
+    chunk = chunk.replace(/\n/g, '');
+    if (chunk !== null) {
+      if (chunk.indexOf('\\kick ') === 0) {
+        chatGroup.forEach((e, i) => {
+          if (e.name === chunk.slice(6)) {
+            e.write('You have been kicked out');
+            e.destroy();
+          }
+        });
+      } else if (chunk.indexOf('kick ip: ') === 0) {
+        chatGroup.forEach((e, i) => {
+          if (e.Port === chunk.slice(9)) {
+            console.log(chunk);
+            e.write('You have been kicked out');
+            e.destroy();
+          }
+        });
+      } else {
+        chatGroup.forEach(e => {
+          e.write(`[ADMIN] : ${chunk}`);
+        });
+      }
     }
-  })
+  });
 });
 
 server.listen(6969, '127.0.0.1', () => {
